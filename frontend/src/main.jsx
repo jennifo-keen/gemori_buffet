821aab60d8ba0c6b4c57782506351d6fb602b1ac
NHATTRUONG265 <160989246+NHATTRUONG265@users.noreply.github.com>
2026-03-05 15:32:07 +0700

    add font be Viet Nam pro

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom' 
+
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
 import router from './router/Router.jsx' 
+import theme from "./theme.js";
 import './main.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </StrictMode>
+);
