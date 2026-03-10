9be0b65154e1f0ebdcb265e3bbe0a9d2788f47db
NHATTRUONG265 <160989246+NHATTRUONG265@users.noreply.github.com>
2026-03-10 18:06:32 +0700

    add web staff

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,13 @@
 const { Router } = require('express');
+
 const router = Router();
 
-// Sẽ thêm dần từng module vào đây
-// router.use('/auth', require('../modules/auth/auth.route'));
+router.use('/auth',      require('../modules/auth/auth.route'));
+router.use('/tables',    require('../modules/tables/tables.route'));
+router.use('/orders',    require('../modules/orders/orders.route'));
+router.use('/customers', require('../modules/customers/customers.route'));
+router.use('/vouchers',  require('../modules/vouchers/vouchers.route'));
+router.use('/payment',   require('../modules/payment/payment.route'));
+router.use('/kitchen',   require('../modules/kitchen/kitchen.route'));
 
 module.exports = router;
