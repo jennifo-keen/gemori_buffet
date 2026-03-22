ce577a78449493def285591ad30769acbd7598bb
NHATTRUONG265 <160989246+NHATTRUONG265@users.noreply.github.com>
2026-03-22 22:19:50 +0700

    change routes staff

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,13 +3,7 @@ const { Router } = require('express');
 const router = Router();
 
 router.use('/auth',      require('../actors/admin/auth/auth.route'));
-router.use('/tables',    require('../modules/tables/tables.route'));
-router.use('/orders',    require('../modules/orders/orders.route'));
-router.use('/customers', require('../modules/customers/customers.route'));
-router.use('/vouchers',  require('../modules/vouchers/vouchers.route'));
-router.use('/payment',   require('../modules/payment/payment.route'));
-router.use('/kitchen',   require('../modules/kitchen/kitchen.route'));
-router.use('/menu',      require('../modules/menu/menu.route'));
-
+router.use('/',    require('../actors/staff/routes/staff.route'));
 router.use('/users', require('../actors/user/auth/customerAuth.route'));
+
 module.exports = router;
