a931c0d94ec2fac25ea07c523853142b284e5ded
NHATTRUONG265 <160989246+NHATTRUONG265@users.noreply.github.com>
2026-03-28 23:08:27 +0700

    orderrouter new file

---
+++ frontend/src/router/OrderRouter.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Layout from "../actors/order/order_page/Order_Layout"
+import Home from "../actors/order/order_page/Order_Home";
+import Menu from "../actors/order/order_page/Order_Menu"
+import Login from "../actors/order/order_page/Order_Login"
+import Cart from "../actors/order/order_page/Order_Cart"
+import Profile from "../actors/order/order_page/Order_Profile"
+import ProfileBuffer from "../actors/order/order_page/Order_ProfileBuffer"
+import Setting from "../actors/order/order_page/Order_Setting"
+
+
+const OrderRouter = [
+ {
+  path: "/order",
+  element: <Layout />,
+  children: [
+    {
+      index: true,
+      element: <Home />,
+    },
+    {
+      path: "menu",
+      element: <Menu />,
+    },
+    {
+      path: "login",
+      element: <Login />,
+    },
+    {
+      path: "cart",
+      element: <Cart />,
+    },
+    {
+      path: "profile",
+      element: <Profile />,
+    },
+    {
+      path: "profilebuffer",
+      element: <ProfileBuffer />,
+    },
+    {
+      path: "set",
+      element: <Setting />,
+    },
+  ],
+},
+];
+
+export default OrderRouter;
