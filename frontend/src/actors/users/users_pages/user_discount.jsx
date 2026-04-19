6d38759bfdea8beb23dc8767d97ed73388070f5a
Phạm Hữu Kiên <kphamhuu2@gmail.com>
2026-04-19 21:56:04 +0700

    responsive trang voucher

--- frontend/src/actors/users/users_pages/user_discount.jsx
+++ frontend/src/actors/users/users_pages/user_discount.jsx
@@ -7,16 +7,32 @@ import { ThemeProvider } from "../users_components/layout/ThemeProvider";
 const Main = () => {
   return (
     <ThemeProvider>
-      <Box component="main" 
-      sx={{ 
-        px: 9, 
-        py: 4,
-        backgroundColor: "#F5F5F5",
-          }}>
-        <Stack direction="row" spacing={4} alignItems="flex-start">
-            <ProfileSidebar/>
+      <Box
+        component="main"
+        sx={{
+          backgroundColor: "#F5F5F5",
+          minHeight: "100vh",
+          py: { xs: 2, sm: 3, md: 4 },
+        }}
+      >
+        <Box
+          sx={{
+            width: "100%",
+            maxWidth: "1280px",
+            mx: "auto",
+            px: { xs: 2, sm: 3, md: 4, lg: 6 },
+            boxSizing: "border-box",
+          }}
+        >
+          <Stack
+            direction={{ xs: "column", md: "row" }}
+            spacing={{ xs: 2, sm: 3, md: 4 }}
+            alignItems="stretch"
+          >
+            <ProfileSidebar />
             <Voucher />
-        </Stack>
+          </Stack>
+        </Box>
       </Box>
     </ThemeProvider>
   );
