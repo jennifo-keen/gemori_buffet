c943c26bb74d1c42d67e8654e0befb95cf4e9283
buynbuyn <lephambaouyen27@gmail.com>
2026-04-20 10:40:38 +0700

    fix url

--- frontend/src/actors/admin/AdLogin.jsx
+++ frontend/src/actors/admin/AdLogin.jsx
@@ -40,7 +40,8 @@ export const AdLogin = () => {
         try {
             setLoading(true);
 
-            const response = await fetch("http://localhost:3000/api/admin/login", {
+            const response = await fetch(`${import.meta.env.VITE_SOCKET_URL}/admin/login`, 
+                {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
